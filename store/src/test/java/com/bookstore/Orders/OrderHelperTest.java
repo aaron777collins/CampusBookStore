6df1e2eac9ec78a9f8505bba02786766c1aa0058
aaron777collins <aaron777collins@gmail.com>
2022-10-31 21:41:08 -0400

    fixed gitignore again and added another test

--- store/src/test/java/com/bookstore/Orders/OrderHelperTest.java
+++ store/src/test/java/com/bookstore/Orders/OrderHelperTest.java
@@ -25,6 +25,15 @@ public class OrderHelperTest {
 
         assertNotEquals(orderID1, orderID2);
 
+        MakeOrderTester tester2 = new MakeOrderTester();
+        Thread thread2 = new Thread(tester2);
+        thread2.start();
+        thread2.join();
+        String orderID3 = tester2.getOrderID1();
+        String orderID4 = tester2.getOrderID2();
+
+        assertNotEquals(orderID3, orderID4);
+
     }
 
     private Order makeOrderTest() {
@@ -53,6 +62,32 @@ class MakeOrderTester implements Runnable{
         }
         List<Item> items2 = new ArrayList<Item>();
         items2.add(new Item("testItem", 22.99f, "1234", ItemType.OTHER));
+        Order order2 = OrderHelper.makeOrder("test", items);
+        orderID1 = order2.orderID;
+    }
+
+
+    public String getOrderID1() {
+        return orderID1;
+    }
+    public String getOrderID2() {
+        return orderID2;
+    }
+}
+
+class MakeOrderTesterSameTime implements Runnable{
+
+    private volatile String orderID1 = null;
+    private volatile String orderID2 = null;
+
+    @Override
+    public void run() {
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("testItem", 22.99f, "1234", ItemType.OTHER));
+        Order order = OrderHelper.makeOrder("test", items);
+        orderID1 = order.orderID;
+        List<Item> items2 = new ArrayList<Item>();
+        items2.add(new Item("testItem2", 22.99f, "1234", ItemType.OTHER));
         Order order2 = OrderHelper.makeOrder("test", items);
         orderID1 = order2.orderID;
     }
