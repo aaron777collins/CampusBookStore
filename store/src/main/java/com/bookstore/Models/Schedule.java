97febba99893ab41e334336d9b35fdbcaca24229
Cameron Ingratta <camingratta@gmail.com>
2022-10-31 20:55:41 -0400

    Test push with schedule and shift class

---
+++ store/src/main/java/com/bookstore/Models/Schedule.java
@@ -0,0 +1,14 @@
+package com.bookstore.Models;
+
+public class Schedule{
+    public Shift shift;
+    public String employeeName;
+    public String day;
+
+    /** 
+     * @param shift contains shift start and end time 
+     * @param employeName name of employee
+     * @param day day of week
+    */
+
+}
---
+++ store/src/main/java/com/bookstore/Models/Shift.java
@@ -0,0 +1,13 @@
+package com.bookstore.Models;
+
+
+public class Shift{
+    int start;
+    int end;
+
+
+    /**
+     * @param start start time of shift
+     * @param end end time of shift
+     */
+}
